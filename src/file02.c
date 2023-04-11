fcecec17c0b95f90bff1b571b3917fdcec63bdaf
Daniela Alexandra Reis Matos <dreis-ma@student.42porto.com>
2023-04-11 19:49:51 +0100

    11 Apr

--- src/file02.c
+++ src/file02.c
@@ -6,37 +6,60 @@
 /*   By: dreis-ma <dreis-ma@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 19:54:33 by dreis-ma          #+#    #+#             */
-/*   Updated: 2023/04/06 20:17:04 by dreis-ma         ###   ########.fr       */
+/*   Updated: 2023/04/11 19:49:51 by dreis-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int check_if_sorted(t_stack *stack)
+void	sort_three(t_stack *stack, int len)
 {
-    while (stack->next)
-    {
-        ft_printf("%i, %i\n", stack->nb, stack->next->nb);
-        if (stack->nb > stack->next->nb)
-            return (0);
-        stack = stack->next;
-    }
-    return (1);
+	int	a;
+	int	b;
+	int c;
+	
+	if (stack->nb > stack->next->nb && stack->next->nb < stack->next->next->nb)
+		swap_a(stack);
+	if (stack->nb < stack->next->nb && stack->next->nb > stack->next->next->nb)
+	{
+		rra(stack, len);
+		swap_a(stack);
+	}
+	
 }
 
-void    sort_list(t_stack *head, int len)
+void	sort_two(t_stack *stack)
 {
-    t_stack	*stack;
-    
-    stack = head;
-    if (check_if_sorted(head) == 1)
-        ft_printf("List is sorted.");
-    else
-    {
-        if (len == 2)
-            sort_two(head);
-        if (len == 3)
-            sort_three(head);
-        ft_printf("List is NOT sorted.");
-    }
+	if (stack->nb > stack->next->nb)
+		swap_a(stack);
+}
+
+int	check_if_sorted(t_stack *stack)
+{
+	while (stack->next)
+	{
+		ft_printf("%i, %i\n", stack->nb, stack->next->nb);
+		if (stack->nb > stack->next->nb)
+			return (0);
+		stack = stack->next;
+	}
+	return (1);
+}
+
+
+void	sort_list(t_stack *head, int len)
+{
+	t_stack	*stack;
+
+	stack = head;
+	if (check_if_sorted(head) == 1)
+		ft_printf("List is sorted.");
+	else
+	{
+		if (len == 2)
+			sort_two(head);
+		if (len == 3)
+			sort_three(head, len);
+		ft_printf("List is NOT sorted.");
+	}
 }
