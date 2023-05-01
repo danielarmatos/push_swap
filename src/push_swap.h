11e5d4703cfcfdacbcf9e400e982e1713dedd0bc
Daniela Alexandra Reis Matos <dreis-ma@student.42porto.com>
2023-05-01 16:44:27 +0100

    Final reviews

--- src/push_swap.h
+++ src/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: dreis-ma <dreis-ma@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 18:25:19 by dreis-ma          #+#    #+#             */
-/*   Updated: 2023/04/27 18:06:56 by dreis-ma         ###   ########.fr       */
+/*   Updated: 2023/05/01 16:41:11 by dreis-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,7 @@ typedef struct s_utils
 	int				nb_index_a;
 	int				best_moves;
 	int				curr_moves;
+	int				curr_a_rev;
 	int				reverse;
 	int				len_a;
 	int				len_b;
@@ -48,7 +49,6 @@ int		*convert_to_int(char **arr, int len);
 int		check_if_numbers(char **arr, int len);
 
 void	create_linked_list(int *int_arr, int len);
-t_stack	*new_node(int nb, t_stack *next_stack);
 
 int		check_if_sorted(t_stack *stack, int len);
 void	sort_two(t_stack **stack);
@@ -68,6 +68,8 @@ int		get_min(t_stack **stack);
 void	sort_big(t_stack **stack_a, t_stack **stack_b);
 void	check_best_move(t_stack **stack_a, t_stack **stack_b);
 void	prepare_to_push(t_stack **stack_a, t_stack **stack_b, t_utils *utils);
+void	print_rotations(t_utils *utils);
+void	print_rotations_02(t_utils *utils);
 
 void	sort_end(t_stack **stack_a);
 t_utils	*setup_utils(t_stack **stack_a, t_stack **stack_b);
--- src/sort_big.c
+++ src/sort_big.c
@@ -6,63 +6,11 @@
 /*   By: dreis-ma <dreis-ma@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 19:27:29 by dreis-ma          #+#    #+#             */
-/*   Updated: 2023/04/27 20:22:37 by dreis-ma         ###   ########.fr       */
+/*   Updated: 2023/05/01 16:36:18 by dreis-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-void	sort_min_max(t_stack **stack_a, t_stack **stack_b, int min, int max)
-{
-	if ((*stack_a)->nb > max || (*stack_a)->nb < min)
-		push(stack_a, stack_b, 'b');
-	else if ((*stack_a)->next->nb > max || (*stack_a)->next->nb < min)
-	{
-		swap(stack_a, 'a');
-		push(stack_a, stack_b, 'b');
-	}
-	else if ((*stack_a)->prev->nb > max || (*stack_a)->prev->nb < min)
-	{
-		reverse_rotate(stack_a, get_stack_len(stack_a), 'a');
-		push(stack_a, stack_b, 'b');
-	}
-	else
-		check_best_move(stack_a, stack_b);
-}
-
-void	sort_default(t_stack **stack_a, t_stack **stack_b)
-{
-	if ((*stack_a)->nb > (*stack_b)->nb
-		&& (*stack_a)->nb < (*stack_b)->prev->nb)
-		push(stack_a, stack_b, 'b');
-	else if ((*stack_a)->next->nb > (*stack_b)->nb
-		&& (*stack_a)->next->nb < (*stack_b)->prev->nb)
-	{
-		swap(stack_a, 'a');
-		push(stack_a, stack_b, 'b');
-	}
-	else if ((*stack_a)->prev->nb > (*stack_b)->nb
-		&& (*stack_a)->prev->nb < (*stack_b)->prev->nb)
-	{
-		reverse_rotate(stack_a, get_stack_len(stack_a), 'a');
-		push(stack_a, stack_b, 'b');
-	}
-	else
-		check_best_move(stack_a, stack_b);
-}
-
-void	start_sorting(t_stack **stack_a, t_stack **stack_b)
-{
-	int	max;
-	int	min;
-
-	while (get_stack_len(stack_a) != 0)
-	{
-		max = get_max(stack_b);
-		min = get_min(stack_b);
-		check_best_move(stack_a, stack_b);
-	}
-}
 
 void	sort_big(t_stack **stack_a, t_stack **stack_b)
 {
@@ -70,7 +18,8 @@ void	sort_big(t_stack **stack_a, t_stack **stack_b)
 	push(stack_a, stack_b, 'b');
 	if ((*stack_b)->nb < (*stack_b)->next->nb)
 		swap(stack_b, 'b');
-	start_sorting(stack_a, stack_b);
+	while (get_stack_len(stack_a) != 0)
+		check_best_move(stack_a, stack_b);
 	while (get_stack_len(stack_b) != 0)
 		push(stack_b, stack_a, 'a');
 	sort_end(stack_a);
